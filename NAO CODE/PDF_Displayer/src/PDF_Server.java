ebbb544c1f55debf68c19a8a96fc22f8c61d7c8f
Liuyi Chai <zoechai229@gmail.com>
2019-05-04 14:59:17 +1000

    PDF Displayer

--- NAO CODE/PDF_Displayer/src/PDF_Server.java
+++ NAO CODE/PDF_Displayer/src/PDF_Server.java
@@ -14,9 +14,11 @@ import javax.swing.JPanel;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridBagLayout;
 import java.awt.Image;
+import java.awt.Toolkit;
 
 import javax.swing.JTextField;
 
@@ -52,6 +54,7 @@ import javax.swing.JScrollPane;
 import javax.swing.JLabel;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.SwingConstants;
 
 import java.awt.Font;
 import java.awt.event.MouseAdapter;
@@ -62,12 +65,17 @@ import java.awt.event.KeyEvent;
 import java.awt.SystemColor;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
 
 import javax.swing.JList;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.util.ImageIOUtil;
+
 public class PDF_Server{
-	private PDF_Display display;
 	private JFrame mainframe;
+	private JLabel imgLabel;
 	public static List<String> bookList = new ArrayList<>();
 	public static JList<String> list = new JList<String>(new DefaultListModel<String>());
 	public static File bookText;
@@ -103,47 +111,73 @@ public class PDF_Server{
 		mainframe = new JFrame();
 		mainframe.getContentPane().setBackground(Color.WHITE);
 		mainframe.getContentPane().setFont(new Font("Cooper Black", Font.BOLD, 19));
-		mainframe.setSize(550, 400);
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		int width = (int)screenSize.getWidth();
+		int height = (int)screenSize.getHeight();
+		
+		mainframe.setSize(width,height);
 		mainframe.setLocationRelativeTo(null);
 		mainframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainframe.getContentPane().setLayout(null);
 		
-	
-		
-		
-		
-		JLabel totalFrame = new JLabel("");
-		totalFrame.setBounds(6, 6, 538, 366);
-		mainframe.getContentPane().add(totalFrame);
-		
-		JLabel lblNewLabel = new JLabel("Waiting for the robot to connect......");
-		lblNewLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
-		lblNewLabel.setBounds(87, 146, 402, 55);
-		mainframe.getContentPane().add(lblNewLabel);
-	
+		imgLabel = new JLabel("Waiting for the robot to connect......",SwingConstants.CENTER);
+		imgLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
+		imgLabel.setBounds(mainframe.getBounds());
+		mainframe.getContentPane().add(imgLabel);
 		
 	}
 	
 	
 	
-		public void requests(Socket socket){
+	public void requests(Socket socket){
 			try {
-				//shift the windows
-				mainframe.dispose();
-				display = new PDF_Display();
-				display.setVisible(true);
+	
+				
+				System.out.println("1");//test 1
 				
 				BufferedReader reader;
 				String line;
 				String clientMsg = null;
 				
 				reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
-
-				while((clientMsg = reader.readLine()) != null) {
-					
-						
-						System.out.println(clientMsg);
-					} 
+                
+				//get the pages and book title
+				String[] pages = getBookPages();
+				List<String> pageArray = new ArrayList<>(); 
+				for(int i=1;i<pages.length;i++){
+					pageArray.add(pages[i]);
+				}
+				
+				String bookTitle = getBookTitle();
+				
+				imgLabel.setText("Connection established. Preparing the book file......");
+				convert(bookTitle);//parse the pdf file
+				
+				System.out.println("2");//test 2
+				
+				
+				//set the book cover as the first page
+				imgLabel.setText("");
+				Image pageImg = new ImageIcon("/C:/Users/Christian Lan/OneDrive/NAO CODE/imgs/1.png").getImage();
+				Image scaledImage = pageImg.getScaledInstance(imgLabel.getWidth(),imgLabel.getHeight(),Image.SCALE_SMOOTH);
+				ImageIcon icon = new ImageIcon(scaledImage);
+				imgLabel.setIcon(icon);
+				
+				System.out.println("3");//test3
+				
+				
+		        //turn page when receiving the "turn" message
+		        while((clientMsg = reader.readLine()) != null) {
+					   int pagenum = Integer.parseInt(pageArray.get(0));
+					   //check if the first chosen page is the book cover
+					   if(pagenum==1){
+						   pageArray.remove(pageArray.get(0));
+						   pagenum = Integer.parseInt(pageArray.get(0));
+					   }
+					   turnPage(pagenum);
+					   pageArray.remove(pageArray.get(0));
+					   System.out.println(clientMsg);
+				} 
 			
 				// TODO Auto-generated catch block
 				
@@ -203,7 +237,98 @@ public class PDF_Server{
 			}
 		}
 	}
+	
+//read the book_pages.txt file to get the pages
+private String[] getBookPages() throws IOException{
+	String[] pages = null;
+	File book_pages = new File("/C:/Users/Christian Lan/OneDrive/NAO CODE/books/book_pages.txt");
+	if(!book_pages.exists()){
+		System.out.println("There is no chosen book.");
+	}else{
+        String lines = null;
+        FileReader fileReader = new FileReader("/C:/Users/Christian Lan/OneDrive/NAO CODE/books/book_pages.txt");
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        int count = 0;
+        while((lines=bufferedReader.readLine()) != null) {
+        	if(count == 1){
+        		String bookPagesStr = "["+lines.trim();
+            	String regex = "\\D+";
+            	pages = bookPagesStr.split(regex);//get a string array of the page numbers
+            	
+            	System.out.println(pages[0]);
+            	
+        	}
+        	count++;
+        }   
 
+        bufferedReader.close();         
+    }
+	return pages;
+}
+
+//read the book_pages.txt file to get the books
+private String getBookTitle() throws IOException{
+		String bookTitle = null;
+		File book_pages = new File("/C:/Users/Christian Lan/OneDrive/NAO CODE/books/book_pages.txt");
+		if(!book_pages.exists()){
+			System.out.println("There is no chosen book.");
+		}else{
+          String line = null;
+          FileReader fileReader = new FileReader("/C:/Users/Christian Lan/OneDrive/NAO CODE/books/book_pages.txt");
+          BufferedReader bufferedReader = new BufferedReader(fileReader);
+          int count = 0;
+          while((line=bufferedReader.readLine()) != null) {
+          	if(count == 0){
+          		bookTitle = line.trim();
+          	}
+          	count++;
+          	
+          	
+          }   
+
+          bufferedReader.close();         
+      }return bookTitle;
+}
+
+//convert the pdf pages to images using pdfbox
+private void convert(String bookTitle){
+		try {
+			
+			File pdfFile = new File("/C:/Users/Christian Lan/OneDrive/NAO CODE/books/"+ bookTitle);
+			if(!pdfFile.exists()){
+				System.out.println("Book not found.");
+			}else{
+				PDDocument document = PDDocument.loadNonSeq(pdfFile, null);
+				List<PDPage> pdPages = document.getDocumentCatalog().getAllPages();
+				int page = 0;
+				
+				File folder = new File("/C:/Users/Christian Lan/OneDrive/NAO CODE/imgs");
+				folder.mkdir();
+				if(folder.exists()==false){
+					folder.createNewFile();
+				}
+				
+				for (PDPage pdPage : pdPages)
+				{ 
+				    ++page;
+				    BufferedImage bim = pdPage.convertToImage(BufferedImage.TYPE_INT_RGB, 300);
+				    ImageIOUtil.writeImage(bim,"png", "/C:/Users/Christian Lan/OneDrive/NAO CODE/imgs/" + page,BufferedImage.TYPE_INT_RGB, 300);
+				}
+				document.close();
+			}
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			///e1.printStackTrace();
+		}
+		}
+//set the pane with images
+private void turnPage(int pagenum){
+	Image pageImg = new ImageIcon("/C:/Users/Christian Lan/OneDrive/NAO CODE/imgs/" + pagenum + ".png").getImage();
+	Image scaledImage = pageImg.getScaledInstance(imgLabel.getWidth(),imgLabel.getHeight(),Image.SCALE_SMOOTH);
+	ImageIcon icon = new ImageIcon(scaledImage);
+	imgLabel.setIcon(icon);
+}
+	
 }
 
 
