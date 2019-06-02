7e849f2320a1a607c3d26687a4b7f94c19188462
Xinjie Lan <xinjielan1@gmail.com>
2019-06-02 17:41:30 +1000

    Delete rest.py

--- NAO CODE/rest.py
+++
@@ -1,9 +0,0 @@
-
-from naoqi import ALProxy
-IP = "172.20.10.14"
-Port = 9559
-motion = ALProxy("ALMotion", IP ,Port)
-motion.rest()
-"""dialog = ALProxy('ALDialog', IP, Port)
-dialog.unloadTopic()
-dialog.unsubscribe('myModule1')"""
